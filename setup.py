794f2f6db6f8ce8e55287f174e34c97c1bced336
Simone Bacchio <s.bacchio@gmail.com>
2020-05-02 13:05:16 +0300

    Running black and adding requirement

--- setup.py
+++ setup.py
@@ -2,21 +2,19 @@ from setuptools import find_packages
 from setuptools import setup
 
 requirements = [
-    
+    "python-varname",
 ]
 
-extras = {
-    "graph": ["graphviz",]
-}
+extras = {"graph": ["graphviz",]}
 
 setup(
-    name = "tunable",
-    author = "Simone Bacchio",
-    author_email = "s.bacchio@gmail.com",
-    url = "https://tunable.readthedocs.io/en/latest",
-    download_url = "https://github.com/sbacchio/tunable",
-    version = "0.0.0",
-    packages = find_packages(),
-    install_requires = requirements,
-    extras_require = extras,
+    name="tunable",
+    author="Simone Bacchio",
+    author_email="s.bacchio@gmail.com",
+    url="https://tunable.readthedocs.io/en/latest",
+    download_url="https://github.com/sbacchio/tunable",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=requirements,
+    extras_require=extras,
 )
