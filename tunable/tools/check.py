e61d72c52b7a86d383b4784a5453bcec1a8a73e0
Simone Bacchio <s.bacchio@gmail.com>
2020-07-03 18:57:00 +0300

    Updating crosscheck

--- tunable/tools/check.py
+++ tunable/tools/check.py
@@ -4,48 +4,31 @@ Function for checking the results
 # pylint: disable=C0303,C0330
 
 import operator
+from numpy import allclose
 from .base import sample
+from ..finalize import finalize
 
 __all__ = [
-    "test",
     "crosscheck",
 ]
 
 
-def test(tunable, fnc, **kwargs):
-    """
-    Tests the results of the tunable object
-    
-    Parameters
-    ----------
-    fnc: callable
-        The function to use for testing the results. It is called as test(params, value)
-        and should return a value from 0 (False) to 1 (True).
-    variables: list of str
-        Set of variables to sample.
-    samples: int
-        The number of samples to run. If None, all the combinations are sampled.
-    kwargs: dict
-        Variables passed to the compute function. See help(tunable.compute)
-    """
-    sampler = sample(tunable, **kwargs)
-
-    yield next(sampler, None)
-
-    for params, value in sampler:
-        if not isinstance(value, Exception):
-            value = fnc(params, value)
-        yield params, value
-
-
-def crosscheck(tunable, fnc=operator.eq, reference=None, **kwargs):
+def crosscheck(
+    tunable,
+    *variables,
+    comparison=allclose,
+    samples=None,
+    reference=None,
+    label="xcheck",
+    **kwargs
+):
     """
     Crosscheck the result of tunable against the reference.
     
     Parameters
     ----------
-    fnc: callable (default = eq)
-        The function to use for comparison. It is called as test(reference, value)
+    comparison: callable (default = numpy.allclose)
+        The function to use for comparison. It is called as fnc(reference, value)
         and should return a value from 0 (False) to 1 (True).
     reference: Any
         The reference value. If None, than the default values are used to produce the result.
@@ -56,16 +39,14 @@ def crosscheck(tunable, fnc=operator.eq, reference=None, **kwargs):
     kwargs: dict
         Variables passed to the compute function. See help(tunable.compute)
     """
-    sampler = sample(tunable, **kwargs)
-
-    variables, tunable = next(sampler, None)
-
     if reference is None:
-        reference = tunable.copy().compute(**kwargs)
+        reference = finalize(tunable).copy().compute(**kwargs)
 
-    yield variables, tunable
-
-    for params, value in sampler:
-        if not isinstance(value, Exception):
-            value = fnc(reference, value)
-        yield params, value
+    return sample(
+        tunable,
+        *variables,
+        callback=lambda res: comparison(reference, res),
+        samples=samples,
+        label=label,
+        **kwargs
+    )
