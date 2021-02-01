b329937770d005bbbe4d6bc60d4c0d3b42ed9b1c
Raphaella Demetriou <ucabrde@ucl.ac.uk>
2021-02-01 18:01:10 +0200

    functions for merging nodes have been added

--- tuneit/finalize.py
+++ tuneit/finalize.py
@@ -2,13 +2,12 @@
 High Level vision of Tunable object
 """
 
-__all__ = [
-    "finalize",
-]
+__all__ = ["finalize"]
 
 from .graph import Node, Key
 from .variable import Variable
 from .tunable import Object, Function, compute, Data
+from .subgraph import Subgraph
 
 
 def finalize(tunable):
@@ -36,6 +35,15 @@ class HighLevel(Node):
         )
 
     @property
+    def direct_variables(self):
+        "List of dependencies that are a variable"
+        return tuple(
+            str(dep)
+            for dep in self.direct_dependencies
+            if isinstance(self[dep], Variable)
+        )
+
+    @property
     def functions(self):
         "List of dependencies that are a functions"
         return tuple(
@@ -59,6 +67,11 @@ class HighLevel(Node):
         if isinstance(value, Object):
             return self.depends_on(value.key)
         return False
+
+    def get_node(self, key):
+        if isinstance(key, Object):
+            key = key.key
+        return HighLevel(self.graph[key])
 
     def __getitem__(self, key):
         if isinstance(key, Object):
@@ -150,3 +163,108 @@ class HighLevel(Node):
         "Computes the result of the Node"
         kwargs.setdefault("graph", self.graph)
         return compute(self.value, **kwargs)
+
+    def merge(self, nodes):
+        "check conditions and return a new graph with the nodes merged in one function"
+        # nodes must be functions, only one output is allowed and they must be consecutive/parallel
+        # new function must ensure that all operations are executed in the correct order
+
+    def remove(self, nodes):
+        "removes the nodes that will be merged from the graph (except the last one)"
+        for node in nodes:
+            del self.graph[node]
+
+    def replace(self, last_node, new_node):
+        "replaces last_node with new_node and changes outside dependencies to the new node"
+        for node in self.graph:
+            list_of_deps = list(
+                str(dep) for dep in self.get_node(node).first_dependencies
+            )
+            for (i, dep) in enumerate(list_of_deps):
+                if dep == last_node:
+                    list(self.get_node(node).first_dependencies)[i].key = str(
+                        new_node.key
+                    )
+        self.graph[str(new_node.key)] = new_node
+
+    def merge(self, nodes):
+        "merges a list of nodes from the graph into a new node"
+        for node in nodes:
+            if not isinstance(self[node], Function):
+                raise ValueError("The node does not represent a function")
+        last_node, merge = self.mergeable(nodes)
+        if not merge:
+            raise ValueError("Group of nodes not mergeable")
+        nodes_values = [self[node] for node in nodes]
+        deps = set(
+            [
+                str(dep)
+                for node in nodes
+                for dep in self.get_node(node).first_dependencies
+            ]
+        )
+        deps = tuple([Key(dep) for dep in list(deps) if dep not in nodes])
+        sub = Subgraph(nodes_values, output=last_node, dependencies=deps)
+        new_node = Function(sub, args=sub.dependencies)
+        new_graph = self.copy(reset=True)
+        nodes.remove(last_node)
+        new_graph.remove(nodes)
+        new_graph.replace(last_node, new_node)
+        return new_graph
+
+    def consecutive(self, nodes):
+        "implements a DFS to check if the undirected graph is connected (if all nodes are consecutive)"
+        stack = [nodes[0]]
+        unvisited = nodes[1:]
+        while stack != []:
+            u = stack[-1]
+            appended = False
+            for w in [str(dep) for dep in self.get_node(u).first_dependencies]:
+                if w in unvisited:
+                    unvisited.remove(w)
+                    stack.append(w)
+                    appended = True
+                    break
+            if appended == False:
+                for w in [
+                    str(node)
+                    for node in nodes
+                    if u in self.get_node(node).first_dependencies
+                ]:
+                    if w in unvisited:
+                        unvisited.remove(w)
+                        stack.append(w)
+                        appended = True
+                        break
+            if appended == False:
+                stack.pop()
+        if unvisited == []:
+            return True
+        return False
+
+    def one_output(self, nodes):
+        "gets a list of nodes and checks that they only produce one output"
+        common = []
+        common_outside = []
+        all_nodes = [
+            str(n) for n in self.dependencies if isinstance(self[str(n)], Function)
+        ]
+        for node in all_nodes:
+            deps = list(
+                set(
+                    [str(dep) for dep in self.get_node(str(node)).first_dependencies]
+                ).intersection(nodes)
+            )
+            if node in nodes:
+                common = common + deps
+            else:
+                common_outside = common_outside + deps
+        if len(set(common_outside)) == 1 and set(common + common_outside) == set(nodes):
+            return common_outside[0], True
+        return common_outside, False
+
+    def mergeable(self, nodes):
+        last_node, res = self.one_output(nodes)
+        if self.consecutive(nodes) and res:
+            return last_node, True
+        return last_node, False
