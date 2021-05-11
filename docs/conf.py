a1de8020eed07c176806abad3df7cb05882bdeb2
Raphaella Demetriou <ucabrde@ucl.ac.uk>
2021-05-11 12:08:13 +0300

    reformatted

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "alabaster"
-html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
+html_sidebars = {
+    "**": ["globaltoc.html", "relations.html", "sourcelink.html", "searchbox.html"]
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
